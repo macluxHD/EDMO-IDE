76f3f7cb5ae44fccfeb94f23d58fc587407b1640
@I6367923 <v.shabanaj@student.maastrichtuniversity.nl>
2025-12-01 18:13:01 +0100

    oops forgot to update this file

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,7 +2,7 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-const localeKeys = ["en", "fr", "de"];
+const localeKeys = ["nl", "en", "fr", "de"];
 
 const resources = {} as Record<string, { translation: Record<string, string> }>;
 
